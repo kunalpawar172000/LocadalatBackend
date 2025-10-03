227efd7a646083e5341d7490a3977cf881382855
kunalpawar172000 <88964219+kunalpawar172000@users.noreply.github.com>
2025-10-03 08:34:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,41 +3,24 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./database/db.js";
 import userRoutes from "./routes/user.js";
-import {authenticateToken} from "./middleware/auth.js";
-//  Load environment variables first
+connectDB();
+
+const app = express();
+
 dotenv.config({ path: "./config/config.env" });
 
+const PORT = process.env.PORT;
+console.log(process.env.PORT);
+app.use(cors({ origin: ["http://localhost:3000", "https://locadalat-frontend.vercel.app","https://locadalat-frontend-dev.vercel.app"] }));
+app.use(express.json());
 
-//  Connect to database
-connectDB()
-    .then((database) => {
-        console.log(
-            "Test SS");
+app.get("/", (req, res) => {
+    res.send("Hello from server");
+});
 
-        //  Create Express app
-        const app = express();
 
-        //  Middleware
-        app.use(cors({
-            origin: "https://locadalat-frontend-dev.vercel.app",
-            credentials: true // allow cookies for cross-domain
-        }));
-        app.use(express.json());
-        // app.use(authenticateToken); // apply auth middleware globally
-        //  Routes
-        app.get("/", (req, res) => {
-            res.send("Hello from server");
-        });
-        app.use("/api/user", userRoutes);
+app.use("/api/user", userRoutes);
 
-        //  Start server
-        const PORT = process.env.PORT || 5000;
-        app.listen(PORT, () => {
-            console.log(`Server is running on http://localhost:${PORT}`);
-        });
-
-    })
-    .catch((err) => {
-        console.error("Database connection failed:", err);
-        process.exit(1); // stop server if DB connection fails
-    });
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
